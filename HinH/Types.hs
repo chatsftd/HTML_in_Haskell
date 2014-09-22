8f94b7be4eeb3b7efbef4a60a125d36a84421f5d
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-09-23 06:39:47 +0900

    add modifyAttr

--- HinH/Types.hs
+++ HinH/Types.hs
@@ -2,6 +2,8 @@
 {-# OPTIONS -Wall -fno-warn-unused-do-bind #-}
 module HInH.Types
 (HTML ()
+,p
+,modifyAttr
 )where
 import Control.Monad.Writer
 
@@ -11,13 +13,21 @@ data TT = Tag_ Tag | Text String
 data Tag = Tag{name :: String, attr :: M.Map String String, inner :: HTML ()} 
 
 class A a where __ :: a -> HTML ()
-instance A String where __ = H . tell . (:[]) . Text
+instance A (HTML ()) where __ = id
 instance A TT where __ = H . tell . (:[])
 instance A Tag where __ = H . tell . (:[]) . Tag_
-instance A (HTML ()) where __ = id
+instance A String where __ = H . tell . (:[]) . Text
+
+class B a where __TT :: a -> TT
+instance B TT where __TT = id
+instance B Tag where __TT = Tag_
+instance B String where __TT = Text
 
 makeTag :: (A t) => String -> t -> Tag
 makeTag tagname inside = Tag{name = tagname, attr = M.empty, inner = __ inside}
 
-p :: (A t) => t -> HTML ()
-p = __ . makeTag "p"
+p :: (A t) => t -> Tag
+p = makeTag "p"
+
+modifyAttr :: (M.Map String String -> M.Map String String) -> Tag -> Tag
+modifyAttr f t@Tag{attr = a} = t{attr = f a}
