4364276b47f4e15c2affcbcf5479ec9226bf6f14
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-09-24 22:53:22 +0900

    change indentation

--- main.hs
+++ main.hs
@@ -9,14 +9,15 @@ main = putStrLn $ printHTML main2
 main2 :: HTML ()
 main2 = do
  __$     div $ do
-  __$     h1 "title1"
-  __$     p "a"
-  __$     p "b"
-  __$     p "c"
-  __$     p "d" 
+  __$        h1 "title1"
+  __$        p "a"
+  __$        p "b"
+  __$        p "c"
+  __$        p "d" 
  __$     div $ do
-  __$     h1 "title2"
-  __$     p "a"
-  __$     p "b"
-  __$     p "c"
-  __$     p "d" 
+  __$        h1 "title2"
+  __$        p "a"
+  __$        p "b"
+  __$        p "c"
+  __$        p "d" 
+  
