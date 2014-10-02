95c90d0be37f49074c2baa9f0fe39af61fa8bd25
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-10-03 00:02:16 +0900

    make Print faster with DList

--- HinH/Print.hs
+++ HinH/Print.hs
@@ -5,7 +5,11 @@ module HinH.Print
 ,indF
 )where
 import HinH.Types
+import Prelude hiding((++))
 import qualified Data.Map as M
+import qualified Data.DList as D
+import Data.DList(DList)
+import Data.Monoid
 
 data Format = Format{indentLevel :: Int} deriving(Show)
 empF :: Format
@@ -14,39 +18,51 @@ empF = Format{indentLevel = -1}
 indF :: Format
 indF = Format{indentLevel = 0}
 
-formHead :: Format -> String
-formHead Format{indentLevel = a} = replicate a '\t'
 
-formFoot :: Format -> String
+type Res = DList Char
+
+class P c where pack :: c a -> DList a
+instance P [] where pack = D.fromList
+instance P DList where pack = id
+
+infixr 5 ++
+
+(++) :: (P a,P b) => a c -> b c -> DList c
+a ++ b = pack a `mappend` pack b
+
+formHead :: Format -> Res
+formHead Format{indentLevel = a} = pack $ replicate a '\t'
+
+formFoot :: Format -> Res
 formFoot Format{indentLevel = a} 
- | a >= 0    = "\n"
- | otherwise = ""
+ | a >= 0    = pack "\n"
+ | otherwise = pack ""
  
 up :: Format -> Format
 up f@Format{indentLevel = a} 
  | a >= 0    = f{indentLevel = a+1}
  | otherwise = f 
 
-printETag :: Format -> EmptyTag -> String
+printETag :: Format -> EmptyTag -> Res
 printETag f ETag{nameE = n, attrE = a}  = formHead f ++ "<" ++ n ++ printAttr f a ++ "/>" ++ formFoot f
 
-printSTag :: Format -> ScriptTag -> String
+printSTag :: Format -> ScriptTag -> Res
 printSTag f STag{nameS = n, attrS = a, innerS = s} = 
  formHead f ++ "<" ++ n ++ printAttr f a ++ ">" ++ formFoot f 
  ++ s ++ 
  formHead f ++ "</" ++ n ++ ">" ++ formFoot f
 
-printTag :: Format -> Tag -> String
+printTag :: Format -> Tag -> Res
 printTag f Tag{name = n, attr = a, inner = h} = 
  formHead f ++ "<" ++ n ++ printAttr f a ++ ">" ++ formFoot f ++ 
  printHTML (up f) h ++ 
  formHead f ++ "</" ++ n ++ ">" ++ formFoot f
 
 toStr :: Format -> HTML () -> String
-toStr = printHTML 
+toStr f ht = D.toList $ printHTML f ht
  
-printHTML :: Format -> HTML () -> String
-printHTML fo h = concatMap f tts
+printHTML :: Format -> HTML () -> Res
+printHTML fo h = mConcatMap f tts
  where 
   f(Tag_ t) = printTag fo t 
   f(ETag_ t) = printETag fo t 
@@ -54,18 +70,21 @@ printHTML fo h = concatMap f tts
   f(Text t) = formHead fo ++ esc t ++ formFoot fo
   tts = rawHTML h
 
-printAttr :: Format -> Attr -> String
+printAttr :: Format -> Attr -> Res
 printAttr _ a  
- | M.null a  = ""
- | otherwise = " " ++ concatMap pAttr (M.toList a) ++ " "
+ | M.null a  = pack ""
+ | otherwise = " " ++ mConcatMap pAttr (M.toList a) ++ " "
  where
   pAttr (nam,str) = nam ++ "=\"" ++ esc str ++ "\""
 
-esc :: String -> String
-esc = concatMap e
+esc :: String -> Res
+esc = mConcatMap (pack . e)
  where
   e '<' = "&lt;"
   e '>' = "&gt;"
   e '"' = "&quot;"
   e '&' = "&amp;"
   e x   = [x]
+  
+mConcatMap :: Monoid c => (a -> c) -> [a] -> c  
+mConcatMap = (mconcat .) . map
